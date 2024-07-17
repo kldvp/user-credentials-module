17a4295cc7dfe86c5ae9d25b000ed0865b7f04fe
Kldv Prasad <Kldv@MacBook-Pro.local>
2024-07-17 12:48:33 +0530

    Add name field in signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -9,6 +9,7 @@ export default function Signup() {
     <Stack spacing={2} className="w-full max-w-xs">
     <h1>Create your account</h1>
     <TextField label="Email" variant="outlined" type="email"></TextField>
+    <TextField label="Name" variant="outlined" type="name"></TextField>
     <TextField label="Password" variant="outlined" type="password"></TextField>
     <Button variant="contained" type="submit">Signup</Button>
     <span>
