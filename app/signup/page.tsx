ccb5def9f26dcff95e4de1850a118d8cb8541e97
kldvp <prasadkodali786@gmail.com>
2024-07-17 13:45:58 +0530

    add comments

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -27,10 +27,10 @@ export default function Signup() {
         const numberRegex = new RegExp('\d');
         const letterRegex = new RegExp('[a-zA-Z]');
         return value 
-            && value.length >= 8 
-            && numberRegex.test(value) 
-            && letterRegex.test(value)
-            && specialCharactersRegex.test(value);
+            && value.length >= 8 // password must be 8 characters minimum
+            && numberRegex.test(value) // should contain atleast 1 number
+            && letterRegex.test(value) // should contain atleast 1 letter
+            && specialCharactersRegex.test(value); // should contain atleast 1 special character
     }
 
     const handleSubmit = async (e: SyntheticEvent) => {
